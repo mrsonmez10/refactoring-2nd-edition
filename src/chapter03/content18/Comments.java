971ae4f149249687fa1298d3bb81b90b76feeca2
frknsnmz <furkan.sonmez@bilgiedu.net>
2023-02-26 16:59:16 +0300

    chapter3 finished

---
+++ src/chapter03/content18/Comments.java
@@ -0,0 +1,8 @@
+package chapter03.content18;
+
+// Do not extend this class see this document // ...
+public class Comments {
+
+    // String pattern2 = "(M:|:D:|:C:|:Q:.*?)([a-zA-Z_\\.0-9]+)";
+
+}
---
+++ src/chapter03/content18/RefuseBeRequest.java
@@ -0,0 +1,12 @@
+package chapter03.content18;
+
+public class RefuseBeRequest {
+
+    /*
+     * Bir alt sınıf, ebeveynlerinden miras aldığı yöntem ve özelliklerden yalnızca
+     * bazılarını kullanıyorsa, hiyerarşinin dışına çıkılmış demektir. Gereksiz
+     * yöntemler basitçe kullanılmayabilir veya yeniden tanımlanabilir ve
+     * istisnalara yol açabilir
+     */
+
+}
