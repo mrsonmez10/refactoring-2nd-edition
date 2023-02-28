61a251102bb526140e85dc78a4e1c9c0fac710fe
frknsnmz <furkan.sonmez@bilgiedu.net>
2023-02-28 08:59:07 +0300

    note for tests

--- src/chapter04/content01/Notes.java
+++ src/chapter04/content01/Notes.java
@@ -6,12 +6,27 @@ public class Notes {
          * Aynı zamanda çözümlerimiz başka şeyleri de bozabilir.
          * Tüm testlerimiz otomatik, hızlı ve kendi sonuçlarını kontrol eder olmalı
          * 
+         * In a real system, I might have thousands of tests. A good test framework
+         * allows me to
+         * run them easily and to quickly see if any have failed. This simple feedback
+         * is essential to
+         * self­testing code. When I work, I’ll be running tests very
+         * frequently—checking progress
+         * with new code or checking for mistakes with refactoring
          * 
          * Refactoring requires tests. If you want to refactor, you have to write tests
          * 
          * Bu bi test kitabı değil ama yine yazarın ve benim eklediğim örnekleri
          * göreceğiz
          * 
+         * 
+         * PROBING THE BOUNDARIES
+         * 
+         * Don’t let the fear that testing can’t catch all bugs stop you from writing
+         * tests that catch most bugs.
+         * 
+         * When you get a bug report, start by writing a unit test that exposes the
+         * bug**
          * 
          */
     }
