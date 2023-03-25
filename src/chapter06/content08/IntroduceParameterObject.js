c43c97dee5b4d7c4c94e4f9be3d6716fa042e807
frknsnmz <furkan.sonmez@bilgiedu.net>
2023-03-25 21:33:42 +0300

    introduce parameter object finished

---
+++ src/chapter06/content08/IntroduceParameterObject.js
@@ -0,0 +1,21 @@
+export function readingsOutsideRange(station, range) {
+  return station.readings.filter((r) => !range.contains(r.temp));
+}
+
+export class NumberRange {
+  constructor(min, max) {
+    this._data = { min: min, max: max };
+  }
+
+  get min() {
+    return this._data.min;
+  }
+
+  get max() {
+    return this._data.max;
+  }
+
+  contains(aNumber) {
+    return aNumber >= this.min && aNumber <= this.max;
+  }
+}
---
+++ test/chapter06/content08/IntroduceParameterObject.test.js
@@ -0,0 +1,34 @@
+import { expect } from "chai";
+import { readingsOutsideRange, NumberRange } from "../../../src/chapter06/content08/IntroduceParameterObject.js";
+
+const station = {
+  name: "ZB1",
+  readings: [
+    { temp: 47, time: "2016-11-10 09:10" },
+    { temp: 53, time: "2016-11-10 09:20" },
+    { temp: 58, time: "2016-11-10 09:30" },
+    { temp: 53, time: "2016-11-10 09:40" },
+    { temp: 51, time: "2016-11-10 09:50" },
+  ],
+};
+
+const operationPlan = {
+  temperatureFloor: 51,
+  temperatureCeiling: 53,
+};
+
+describe("readingsOutsideRange", () => {
+  it("give the reading outside temperature outside range", () => {
+    const range = new NumberRange(
+      operationPlan.temperatureFloor,
+      operationPlan.temperatureCeiling
+    );
+
+    let alerts = readingsOutsideRange(station, range);
+
+    expect(alerts).to.eql([
+      { temp: 47, time: "2016-11-10 09:10" },
+      { temp: 58, time: "2016-11-10 09:30" },
+    ]);
+  });
+});
