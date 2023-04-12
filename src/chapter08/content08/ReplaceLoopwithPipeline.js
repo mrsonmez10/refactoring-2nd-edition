47222eff6e0338249fb3d5d2f88ec5d2f7f9aa71
frknsnmz <furkan.sonmez@bilgiedu.net>
2023-04-13 00:34:15 +0300

    replace loop with pipeline finished.

---
+++ src/chapter08/content08/ReplaceLoopwithPipeline.js
@@ -0,0 +1,12 @@
+export function acquireData(input) {
+  const lines = input.split("\n");
+  return lines
+    .splice(1)
+    .filter((line) => line.trim() !== "")
+    .map((line) => line.split(","))
+    .filter((lineItems) => lineItems[1].trim() === "India")
+    .map((lineItems) => ({
+      city: lineItems[0].trim(),
+      phone: lineItems[2].trim(),
+    }));
+}
