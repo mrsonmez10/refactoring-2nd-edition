232f41cdb840ce2c16d6e7c9c3a567a8a1345268
frknsnmz <furkan.sonmez@bilgiedu.net>
2023-04-16 21:38:42 +0300

    Replace Condition with polymorphism finished.

---
+++ src/chapter10/content03/ReplaceConditionalWithPolymorphism1.js
@@ -0,0 +1,74 @@
+export function plumages(birds) {
+  return new Map(birds.map((b) => [b.name, plumage(b)]));
+}
+
+export function speeds(birds) {
+  return new Map(birds.map((b) => [b.name, airSpeedVelocity(b)]));
+}
+
+export function plumage(bird) {
+  return createBird(bird).plumage;
+}
+
+export function airSpeedVelocity(bird) {
+  return createBird(bird).airSpeedVelocity;
+}
+
+class Bird {
+  constructor(birdObject) {
+    Object.assign(this, birdObject);
+  }
+
+  get plumage() {
+    return "unknown";
+  }
+
+  get airSpeedVelocity() {
+    return null;
+  }
+}
+
+function createBird(bird) {
+  switch (bird.type) {
+    case "EuropeanSwallow":
+      return new EuropeanSwallow(bird);
+    case "AfricanSwallow":
+      return new AfricanSwallow(bird);
+    case "NorwegianBlueParrot":
+      return new NorwegianBlueParrot(bird);
+    default:
+      return new Bird(bird);
+  }
+}
+
+class EuropeanSwallow extends Bird {
+  get plumage() {
+    return "average";
+  }
+
+  get airSpeedVelocity() {
+    return 35;
+  }
+
+  // ...
+}
+
+class AfricanSwallow extends Bird {
+  get plumage() {
+    return this.numberOfCoconuts > 2 ? "tired" : "average";
+  }
+
+  get airSpeedVelocity() {
+    return 40 - 2 * this.numberOfCoconuts;
+  }
+}
+
+class NorwegianBlueParrot extends Bird {
+  get plumage() {
+    return this.voltage > 100 ? "scorched" : "beautiful";
+  }
+
+  get airSpeedVelocity() {
+    return this.isNailed ? 0 : 10 + this.voltage / 10;
+  }
+}
