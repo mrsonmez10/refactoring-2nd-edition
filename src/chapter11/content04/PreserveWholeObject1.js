49075973fd3b1b2781918184d2aff09498cdb718
frknsnmz <furkan.sonmez@bilgiedu.net>
2023-04-19 01:03:46 +0300

    preserve whole object finished.

---
+++ src/chapter11/content04/PreserveWholeObject1.js
@@ -0,0 +1,21 @@
+export function temperatureAlerts(aRoom, aPlan) {
+  const alerts = [];
+  if (!aPlan.withinRange(aRoom.daysTempRange)) {
+    alerts.push("room temperature went outside range");
+  }
+
+  return alerts;
+}
+
+export class HeatingPlan {
+  constructor(temperatureRange) {
+    this._temperatureRange = temperatureRange;
+  }
+  
+  withinRange(aNumberRange) {
+    return (
+      aNumberRange.low >= this._temperatureRange.low &&
+      aNumberRange.high <= this._temperatureRange.high
+    );
+  }
+}
---
+++ src/chapter11/content04/PreserveWholeObject2.js
@@ -0,0 +1,24 @@
+export function temperatureAlerts(aRoom, aPlan) {
+  const alerts = [];
+  let tempRange = aRoom.daysTempRange;
+
+  let isWithinRange = aPlan.withinRange(tempRange);
+  if (!isWithinRange) {
+    alerts.push("room temperature went outside range");
+  }
+
+  return alerts;
+}
+
+export class HeatingPlan {
+  constructor(temperatureRange) {
+    this._temperatureRange = temperatureRange;
+  }
+
+  withinRange(tempRange) {
+    return (
+      tempRange.low >= this._temperatureRange.low &&
+      tempRange.high <= this._temperatureRange.high
+    );
+  }
+}
