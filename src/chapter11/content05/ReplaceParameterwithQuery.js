92c80c24ffa25a5e89e91ccf804e90f00865ae5a
frknsnmz <furkan.sonmez@bilgiedu.net>
2023-04-19 01:27:32 +0300

    Replace parameter query and viceversa finished.

---
+++ src/chapter11/content05/ReplaceParameterwithQuery.js
@@ -0,0 +1,24 @@
+export class Order {
+  constructor(quantity, itemPrice) {
+    this.quantity = quantity;
+    this.itemPrice = itemPrice;
+  }
+
+  get finalPrice() {
+    const basePrice = this.quantity * this.itemPrice;
+    return this.discountedPrice(basePrice, this.discountLevel());
+  }
+
+  discountLevel() {
+    return this.quantity > 100 ? 2 : 1;
+  }
+
+  discountedPrice(basePrice, discountLevel) {
+    switch (discountLevel) {
+      case 1:
+        return basePrice * 0.95;
+      case 2:
+        return basePrice * 0.9;
+    }
+  }
+}
---
+++ src/chapter11/content05/ReplaceQueryWithParameter.js
@@ -0,0 +1,12 @@
+// This To
+targetTemperature(aPlan);
+function targetTemperature(aPlan) {
+  currentTemperature = thermostat.currentTemperature;
+  // rest of function...
+}
+
+// This
+targetTemperature(aPlan, thermostat.currentTemperature);
+function targetTemperature(aPlan, currentTemperature) {
+  // rest of function...
+}
