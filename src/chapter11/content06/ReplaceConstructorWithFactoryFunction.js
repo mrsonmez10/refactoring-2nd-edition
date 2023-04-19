91a8f5423ad0c55a964347f53b7b82fce208b2f8
frknsnmz <furkan.sonmez@bilgiedu.net>
2023-04-19 21:44:41 +0300

    replace constructor with factory function finished.

---
+++ src/chapter11/content06/ReplaceConstructorWithFactoryFunction.js
@@ -0,0 +1,26 @@
+export class Employee {
+  constructor(name, typeCode) {
+    this._name = name;
+    this._typeCode = typeCode;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get type() {
+    return Employee.legalTypeCodes[this._typeCode];
+  }
+
+  static get legalTypeCodes() {
+    return { E: "Engineer", M: "Manager", S: "Salesman" };
+  }
+}
+
+export function createEmployee(name, typeCode) {
+  return new Employee(name, typeCode);
+}
+
+export function createEngineer(name) {
+  return new Employee(name, "E");
+}
---
+++ test/chapter11/content06/ReplaceConstructorWithFactoryFunction.test.js
@@ -0,0 +1,21 @@
+import { expect } from "chai";
+import {
+  createEmployee,
+  createEngineer,
+} from "../../../src/chapter11/content06/ReplaceConstructorWithFactoryFunction.js";
+
+describe("Employee", () => {
+  it("create a Manager with the employee name ", () => {
+    const candidate = createEmployee("Sarah", "M");
+
+    expect(candidate.name).to.equal("Sarah");
+    expect(candidate.type).to.equal("Manager");
+  });
+
+  it("create an Engineer with the employee name ", () => {
+    const candidate = createEngineer("Chloe");
+
+    expect(candidate.name).to.equal("Chloe");
+    expect(candidate.type).to.equal("Engineer");
+  });
+});
